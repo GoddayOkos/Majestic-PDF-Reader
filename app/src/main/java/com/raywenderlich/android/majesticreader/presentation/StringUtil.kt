4e7b9d96ad713e45c9e8a24bfecb809e09760087
Godday Okoduwa <goddayokos@gmail.com>
2021-08-19 23:50:19 +0100

    Created a new core module for the Core Layer implementation

--- app/src/main/java/com/raywenderlich/android/majesticreader/presentation/StringUtil.kt
+++ app/src/main/java/com/raywenderlich/android/majesticreader/presentation/StringUtil.kt
@@ -31,6 +31,8 @@
 package com.raywenderlich.android.majesticreader.presentation
 
 import java.text.DecimalFormat
+import kotlin.math.log10
+import kotlin.math.pow
 
 object StringUtil {
 
@@ -44,10 +46,10 @@ object StringUtil {
     }
 
     val units = arrayOf("B", "kB", "MB", "GB", "TB")
-    val digitGroups = (Math.log10(size.toDouble()) / Math.log10(1024.0)).toInt()
+    val digitGroups = (log10(size.toDouble()) / log10(1024.0)).toInt()
 
     return DecimalFormat("#,##0.#").format(
-        size / Math.pow(1024.0, digitGroups.toDouble())
+        size / 1024.0.pow(digitGroups.toDouble())
     ) + " " + units[digitGroups]
   }
 }
